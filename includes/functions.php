136eab494f2002f498cded7b02037dab79a9bf4f
macbook <macbook@macbooks-MacBook-Pro.local>
2020-11-11 13:36:48 +0200

    adding thumbs

--- includes/functions.php
+++ includes/functions.php
@@ -1,14 +1,27 @@
 <?php 
-    include("connect.php");
-
-    $query = "SELECT * FROM dishesData";
-
-    $runQuery = $pdo->query($query);
-
     $result = array();
 
-    while($row = $runQuery->fetch(PDO::FETCH_ASSOC)) {
-        $result[] = $row;
-    }
+    function getAllUsers($conn){
 
-    echo(json_encode($result));
+        $query = "SELECT * FROM dishesData";
+
+        $runQuery = $conn->query($query);
+
+        while($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
+            $result[] = $row;
+        }
+
+        echo(json_encode($result[0]));
+    }
+    function getSingleUser($conn, $id)
+    {
+        $query = "SELECT * FROM dishesData WHERE id=" . $id . "";
+
+        $runQuery = $conn->query($query);
+
+        while ($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
+            $result[] = $row;
+        }
+
+        echo(json_encode($result[0]));
+    }
--- js/main.js
+++ js/main.js
@@ -25,10 +25,30 @@ import { fetchData } from "./modules/TheDataMiner.js";
 
             dishesSection.appendChild(currentDishes);
         }
+    }
+    function retrieveProjectInfo(){
+        console.log(this.id);
 
-        console.log(data);
+        fetchData(`./includes/index.php?id=${this.id}`).then(data => console.log(data)).catch(err => console.log(err));
     }
 
-    fetchData("./includes/functions.php").then(data => handleDataSet(data)).catch(err => { console.log(err); });
+    function renderDishesThumbnails(thumbs){
+        let dishesSection = document.querySelector('.dishes-section'),
+            dishesTemplate = document.querySelector('#dishes-template').content;
+
+        // loop through the JavaScript object and for each user, make a copy of the user template we find at the bottom of index.html, populate it with the user's data, and put that fresh copy in the users section in index.html
+
+        for (let dishes in thumbs) {
+            let currentDishes = dishesTemplate.cloneNode(true),
+                currentDishesText = currentDishes.querySelector('.dishes').children;
+
+            currentDishesText[1].src = `images/${thumbs[dishes].photo}`;
+            currentDishesText[1].id = thumbs[dishes].id;
+            currentDishes.addEventListener("click", retrieveProjectInfo);
+            dishesSection.appendChild(currentDishes);
+        }
+    }
+
+    fetchData("./includes/index.php").then(data => renderDishesThumbnails(data)).catch(err =>console.log(err));
 
 })();
